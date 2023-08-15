f7a13c5d5b2290708a3bcafbe688cd494c0716e4
elseptiawan <septiawanelshandi@gmail.com>
2023-08-16 00:54:50 +0700

    add back button

--- src/pages/VideoDetail/VideoDetail.jsx
+++ src/pages/VideoDetail/VideoDetail.jsx
@@ -59,6 +59,7 @@ const VideoDetail = () => {
                         playing={true}
                     />
                 </div>
+                <a href="/"><div className={style.back_button}>Back</div></a>
                 <div className={style.right}>
                     <span>Username</span>
                     <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" />
