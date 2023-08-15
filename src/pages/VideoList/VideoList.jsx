0eab13a73e8c43efef80c8e5285d90b4941e8dee
elseptiawan <septiawanelshandi@gmail.com>
2023-08-16 00:44:42 +0700

    Video List page

--- src/pages/VideoList/VideoList.jsx
+++ src/pages/VideoList/VideoList.jsx
@@ -1,20 +1,54 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import style from './videolist.module.css';
 import Thumbnail from '../../components/Thumbnail/Thumbnail';
+import axios from 'axios';
 
 const VideoList = () => {
+  // let {data, error} = useFetch('/video?search=');
+  const [dataVideo, setDataVideo] = useState([]);
+  const [search, setSearch] = useState('');
+
+  const getVideos = async (search) => {
+    const response = await axios.get(`/video?search=${search}`);
+    setDataVideo(response.data.data);
+  }
+
+  const onChangeHandle = (e) => {
+    setSearch(e.target.value);
+  } 
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    console.log(search);
+    getVideos(search);
+  }
+
+  useEffect(() => {
+    getVideos('');
+}, []);
   return (
     <div>
-        <h2 className={style.title}>Video List</h2>
+      <div className={style.header}>
+        <div className={style.left}>
+          <h2 className={style.title}>Video List</h2>
+          <form onSubmit={handleSearch}>
+            <input type="search" placeholder='Cari Video' value={search} onChange={onChangeHandle}/>
+          </form>
+        </div>
+        <div className={style.right}>
+          <span>Username</span>
+          <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" />
+        </div>
+      </div>
         <div className={style.thumbnail_wrapper}>
-            <Thumbnail 
-                url = "https://img.youtube.com/vi/3rlQ-2gsPC0/hqdefault.jpg"
-            />
-            <Thumbnail/>
-            <Thumbnail/>
-            <Thumbnail/>
-            <Thumbnail/>
-            <Thumbnail/>
+        {dataVideo.map((item) => (
+          <Thumbnail 
+            key = {item._id}
+            id = {item._id}
+            url = {item.urlImageThumbnail}
+            judul = {item.title}
+          />
+        ))}
         </div>
     </div>
   )
